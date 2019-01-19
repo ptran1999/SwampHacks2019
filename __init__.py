1987f72a824b477d67d35259a48d0e455e2b3b5a
Phat Tran <34192355+ptran1999@users.noreply.github.com>
2019-01-18 21:21:13 -0500

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-from nutri import route
