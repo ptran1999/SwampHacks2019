55d0c832e56ce8f42dd8046759241e3db32ae0e7
Phat Tran <34192355+ptran1999@users.noreply.github.com>
2019-01-18 21:19:31 -0500

    all other file
    init is the constructure, models is the class for db, rout for each page, site is db file

---
+++ models.py
@@ -0,0 +1,13 @@
+from nutri import db
+
+class Food(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), unique=True, nullable=False)
+	image_file = db.Column(db.String(50), unique=True, nullable=False, default='default.jpg')
+	cal_per_unit = db.Column(db.Integer)
+	protein_per_unit = db.Column(db.Integer)
+	fat_per_unit = db.Column(db.Integer)
+	carb_per_unit = db.Column(db.Integer)
+
+	def __repr__(self):
+		return f"Food('{self.name}','{self.image_file}','{self.cal_per_unit}','{self.protein_per_unit}','{self.fat_per_unit}','{self.carb_per_unit}')"
