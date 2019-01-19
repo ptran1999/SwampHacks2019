04ea634d59fc0fd81049091be05a0003dcb16e57
Phat Tran <34192355+chuoibuoituoi@users.noreply.github.com>
2019-01-19 01:11:17 -0500

    change in route.py and admin.html
    can add info from admin.html to database now

--- nutri/route.py
+++ nutri/route.py
@@ -1,9 +1,18 @@
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for
 from nutri import app, db
 from nutri.models import Food
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	if request.method == 'POST':
+		temp = Food(name=request.form['food_name'], 
+				image_file=request.form['Image'], 
+				cal_per_unit=request.form['cal'], 
+				protein_per_unit=request.form['protein'], 
+				fat_per_unit=request.form['fat'], 
+				carb_per_unit=request.form['carb'])
+		db.session.add(temp)
+		db.session.commit()
 	food_l = Food.query.all()
 	return render_template('index.html', food_l=food_l)
 
@@ -11,12 +20,6 @@ def index():
 def result():
 	return render_template('result.html')
 
-@app.route('/admin')
+@app.route('/admin',methods=['GET', 'POST'])
 def admin():
-	name = request.form['Name']
-	cal = request.form['cal']
-	protein = request.form['protein']
-	fat = request.form['fat']
-	carb = request.form['carb']
 	return render_template('admin.html')
-
